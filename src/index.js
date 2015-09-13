40a1680e0999d27c9744dc5b5d5221902d92f9cc
M. Maxwell Watson <hi@mmwtsn.com>
2015-09-12 23:50:13 -0400

    Add, test Runner#run()

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 /**
+ * Module dependencies
+ */
+
+import {spawn} from 'child_process'
+
+/**
  * Module exports
  */
 
 export default class Runner {
+  run () {
+    spawn('npm', ['test', '-s'], {
+      stdio: [process.stdin, process.stdout, process.stderr]
+    })
+  }
 }
--- test/index.js
+++ test/index.js
@@ -1,6 +1,8 @@
 /* global describe, it */
 
 import assert from 'assert'
+import child_process from 'child_process'
+import sinon from 'sinon'
 import Runner from '../src'
 
 describe('Runner', () => {
@@ -11,5 +13,17 @@ describe('Runner', () => {
     assert.strictEqual(runner.constructor, Runner)
 
     done()
+  })
+
+  describe('#run', () => {
+    it('calls child_process.spawn once', done => {
+      const stub = sinon.stub(child_process, 'spawn')
+      const runner = new Runner()
+
+      runner.run()
+      assert(stub.calledOnce)
+
+      done()
+    })
   })
 })
