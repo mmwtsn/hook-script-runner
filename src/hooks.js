453c17e3f9310e04690f76f40868034e9a0a4a43
M. Maxwell Watson <hi@mmwtsn.com>
2015-09-13 13:10:27 -0400

    Create hooks module containing list of valid hooks

---
+++ src/hooks.js
@@ -0,0 +1,29 @@
+/**
+ * List of all currently hooks recognized by Git v2.5.0
+ *
+ * See `git hooks --help` for full documentation.
+ *
+ * @module src/hooks
+ * @type {string[]}
+ */
+
+export default [
+  'applypatch-msg',
+  'pre-applypatch',
+  'post-applypatch',
+  'pre-commit',
+  'prepare-commit-msg',
+  'commit-msg',
+  'post-commit',
+  'pre-rebase',
+  'post-checkout',
+  'post-merge',
+  'pre-push',
+  'pre-receive',
+  'update',
+  'post-receive',
+  'post-update',
+  'push-to-checkout',
+  'pre-auto-gc',
+  'post-rewrite'
+]
