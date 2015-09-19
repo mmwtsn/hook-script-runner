58e4bc283a3fd6c9a7a4f2d2a71d684076fe6ee8
M. Maxwell Watson <hi@mmwtsn.com>
2015-09-18 23:23:21 -0400

    Document _inspect() protected method

--- src/Installer.js
+++ src/Installer.js
@@ -12,10 +12,31 @@ import fs from 'fs'
  */
 
 export default class Installer {
+  /**
+   * Constructs an instance of Installer
+   *
+   * @param {string} [path=../../package.json] - Path to $GIT_DIR
+   * @constructs Installer
+   */
   constructor (path = '../.git') {
     this.symlinked = this._inspect(`${path}/hooks`, 'isSymbolicLink')
     this.saved = this._inspect(`${path}/hooks.save`, 'isDirectory')
   }
+
+  /**
+   * Returns installation state of provided $GIT_DIR
+   *
+   * Internally constructs a call to `fs.lstat()` with the provided path and
+   * inspects the file stats with by calling the provided method on the returned
+   * stats object. For the full list of available methods see {@link
+   * https://nodejs.org/api/fs.html#fs_class_fs_stats}.
+   *
+   * @method _inspect
+   * @param {string} path - Relative path to $GIT_DIR
+   * @param {string} method - Method called on fs.Stats object
+   * @returns {Boolean} Result of requested check
+   * @protected
+   */
 
   _inspect (path, method) {
     try {
