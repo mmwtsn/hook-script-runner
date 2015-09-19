26763180322becc5d8326f9aa5d3e7387d972e7b
M. Maxwell Watson <hi@mmwtsn.com>
2015-09-18 23:20:16 -0400

    Add, test #symlinked(), #saved() getter methods

--- src/Installer.js
+++ src/Installer.js
@@ -1,4 +1,10 @@
 /**
+ * Module dependencies
+ */
+
+import fs from 'fs'
+
+/**
  * Hook script installer class
  *
  * @class Installer
@@ -6,4 +12,16 @@
  */
 
 export default class Installer {
+  constructor (path = '../.git') {
+    this.symlinked = this._inspect(`${path}/hooks`, 'isSymbolicLink')
+    this.saved = this._inspect(`${path}/hooks.save`, 'isDirectory')
+  }
+
+  _inspect (path, method) {
+    try {
+      return Boolean(fs.lstatSync(path)[method]())
+    } catch (err) {
+      return false
+    }
+  }
 }
